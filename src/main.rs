89ca6cb8a6a131635cd8d75a93cbe79cb89a10c5
Ryan Mullins <rmullins@aptima.com>
2020-12-12 16:10:39 -0500

    Day 5 solutions

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 use day1::solve_d1;
 use day2::solve_d2;
 use day3::solve_d3;
 use day4::solve_d4;
+use day5::solve_d5;
 
 fn main() {
     solve_d1();
     solve_d2();
     solve_d3();
     solve_d4();
+    solve_d5();
 }
