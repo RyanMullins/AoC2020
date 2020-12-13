5c717e4e0a2dc4e5a7807293b56eddcde249858a
Ryan Mullins <rmullins@aptima.com>
2020-12-12 21:21:37 -0500

    Day 6 solutions

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 use day1::solve_d1;
 use day2::solve_d2;
 use day3::solve_d3;
 use day4::solve_d4;
 use day5::solve_d5;
+use day6::solve_d6;
 
 fn main() {
     solve_d1();
@@ -16,4 +18,5 @@ fn main() {
     solve_d3();
     solve_d4();
     solve_d5();
+    solve_d6();
 }
